c9576829b97ca7e56250cd7d85fef219ec1bb65e
info@prime-ltd.su <polimer@server.polimer-vrn.ru>
2019-11-15 14:40:12 +0300

    srv

--- bitrix/templates/main/footer.php
+++ bitrix/templates/main/footer.php
@@ -327,6 +327,16 @@
 	false
 );?>
 
+<script>
+(function(w, d, s, h, id) {
+    w.roistatProjectId = id; w.roistatHost = h;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', '0e03e67d2cf7ac55a00f173bca769e45');
+</script>
+
+
 
 	</body>
 </html>
