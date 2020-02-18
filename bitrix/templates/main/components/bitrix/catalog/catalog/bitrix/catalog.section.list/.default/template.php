79b64b152f7b96adcbf3659ebd18c233d778ac8c
neeil1990 <neeil@mail.ru>
2020-02-18 12:34:50 +0300

    no message

--- bitrix/templates/main/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/template.php
+++ bitrix/templates/main/components/bitrix/catalog/catalog/bitrix/catalog.section.list/.default/template.php
@@ -11,45 +11,22 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
-
-$arViewModeList = $arResult['VIEW_MODE_LIST'];
-
-$arViewStyles = array(
-	'LIST' => array(
-		'CONT' => 'bx_sitemap',
-		'TITLE' => 'bx_sitemap_title',
-		'LIST' => 'bx_sitemap_ul',
-	),
-	'LINE' => array(
-		'CONT' => 'bx_catalog_line',
-		'TITLE' => 'bx_catalog_line_category_title',
-		'LIST' => 'bx_catalog_line_ul',
-		'EMPTY_IMG' => $this->GetFolder().'/images/line-empty.png'
-	),
-	'TEXT' => array(
-		'CONT' => 'bx_catalog_text',
-		'TITLE' => 'bx_catalog_text_category_title',
-		'LIST' => 'bx_catalog_text_ul'
-	),
-	'TILE' => array(
-		'CONT' => 'bx_catalog_tile',
-		'TITLE' => 'bx_catalog_tile_category_title',
-		'LIST' => 'bx_catalog_tile_ul',
-		'EMPTY_IMG' => $this->GetFolder().'/images/tile-empty.png'
-	)
-);
-$arCurView = $arViewStyles[$arParams['VIEW_MODE']];
 ?>
 <?if($arResult['SECTIONS']):?>
-<div class="catalog-sections-list cl">
-	<?foreach ($arResult['SECTIONS'] as &$arSection):?>
+<div class="product_top cl">
 
-	<div class="catalog-sections-list__item">
-		<a href="<?=$arSection['SECTION_PAGE_URL']?>" class="link">
-            <img alt="<?=$arSection['NAME']?>" src="<?=resizeImage($arSection['PICTURE']['ID'], 190, 120);?>" height="120" class="img">
-			<span class="name"><?=$arSection['NAME'];?></span></a>
-	</div>
-	<?endforeach;?>
+    <div class="catalog_top cl">
+        <? foreach($arResult['SECTIONS'] as &$arSection):?>
+            <div class="item_c">
+                <a href="<?=$arSection['SECTION_PAGE_URL']?>">
+                    <div class="img_c">
+                        <img src="<?=resizeImage($arSection['PICTURE']['ID'], 140, 120);?>" alt="<?=$arSection['NAME']?>">
+                    </div>
+                    <div class="name_c"><?=$arSection['NAME']?></div>
+                </a>
+            </div>
+        <? endforeach; ?>
+    </div>
 
 </div>
 <!--end::catalog-sections-->
